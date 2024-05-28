1ec2e6eb7e486fec2269f81b6793d5a177dc87a2
rishabh417 <rishusingh417@gmail.com>
2024-05-28 21:22:03 +0530

    resolved issue of logo not showing

--- src/Logo.jsx
+++ src/Logo.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
+
 export default function Logo({width="100px"}) {
     return(
         <div>
-            <img src="src\logoforblog.PNG" alt="Blog" className="w-[160px] invert"/>
+            <img src="/logoforblog.PNG" alt="Blog" className="w-[160px] invert"/>
         </div>
     )
 }
